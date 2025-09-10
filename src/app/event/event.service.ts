5d887ef5d7aaa2fa73078ba51afa1bf012b0010d
rojiwon123 <dev.rojiwon@gmail.com>
2025-09-11 06:58:30 +0900

    event client 개선

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -1,12 +1,20 @@
 import { WorkerEvent } from "@/common/event/event";
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { lastValueFrom } from "rxjs";
 import { WorkerToken } from "./token";
 
 @Injectable()
-export class EventService {
+export class EventService implements OnModuleInit, OnModuleDestroy {
     constructor(@Inject(WorkerToken) private readonly workerClient: ClientProxy) {}
+
+    async onModuleInit() {
+        await this.workerClient.connect();
+    }
+
+    async onModuleDestroy() {
+        await this.workerClient.close();
+    }
 
     async emitWorker(input: WorkerEvent) {
         await lastValueFrom(this.workerClient.emit(input.name, input.payload));
