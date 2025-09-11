b71caacbd84866fe25d2075398e5a0a68290a72c
rojiwon123 <dev.rojiwon@gmail.com>
2025-09-11 23:38:53 +0900

    문서화 환경 세팅

---
+++ nestia.config.ts
@@ -0,0 +1,15 @@
+import type { INestiaConfig } from "@nestia/sdk";
+
+const NESTIA_CONFIG: INestiaConfig = {
+    input: "src/controller",
+    swagger: {
+        openapi: "3.1",
+        output: "doc/openapi/openapi.json",
+        decompose: true,
+        security: { bearer: { type: "http", scheme: "bearer" } },
+        servers: [{ url: "http://localhost:4000", description: "Local Server" }],
+        beautify: false,
+    },
+};
+
+export default NESTIA_CONFIG;
